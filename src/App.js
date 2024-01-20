e41925600b9273d9898b46bdea038ea628cbccc0
shahrukh <shahrukhbutt804@gmail.com>
2024-01-20 22:15:26 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,150 +1,138 @@
 // import { Button } from 'react-bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { useEffect, useState } from 'react';
-import LoadingBar from 'react-top-loading-bar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useEffect, useState } from "react";
+import LoadingBar from "react-top-loading-bar";
 
 function App() {
-
-
-
-
-
-  let [index, setindex] = useState(0)
-  let [Questions, setQuestion] = useState([]);
+  let [index, setindex] = useState(0);
+  let [Questions, setQuestion] = useState();
   let [isLoading, setloader] = useState(true);
-  let [score, setscore] = useState(0)
-  let [isFinished, setfinished] = useState(false)
-  let [isclicked, setclicked] = useState(false)
-  let [progress, setProgress] = useState(0)
+  let [score, setscore] = useState(0);
+  let [isFinished, setfinished] = useState(false);
+  let [isclicked, setclicked] = useState(false);
+  let [progress, setProgress] = useState(0);
+  let [options, setOptions] = useState([]);
 
-
-                                       //to fetch
-
+  //to fetch
 
   useEffect(() => {
+    shuffledOptopns();
+  }, [index, Questions]);
 
-    setloader(true)
-    fetch('https://the-trivia-api.com/v2/questions/')
-      .then(result => result.json())
-      .then(data => {
-        console.log("data :", data)
-        setQuestion(data)
-        setloader(false)
-      })
-  }, [])
+  useEffect(() => {
+    setloader(true);
+    fetch("https://the-trivia-api.com/v2/questions/")
+      .then((result) => result.json())
+      .then((data) => {
+        console.log("data :", data);
+        setQuestion(data);
+        setloader(false);
+      });
+  }, []);
 
-
-
-
-                                       //to Check
-
-
+  //to Check
 
   const checkAns = (e) => {
+    const ans = Questions[index].correctAnswer;
     if (!isclicked) {
       if (e.target.innerText == Questions[index].correctAnswer) {
         e.target.classList.add("correct");
-        setscore(score + 1)
+        setscore(score + 1);
       } else {
-        e.target.classList.add("galat")
-  
+        e.target.classList.add("galat");
       }
 
-      setclicked(true)
+      setclicked(true);
     }
+  };
 
-
-  }
-
-
-                                       //to next
-
+  const shuffledOptopns = () => {
+    console.log(Questions, "-> questions");
+    if (Questions) {
+      setOptions(
+        [
+          ...Questions[index]?.incorrectAnswers,
+          Questions[index]?.correctAnswer,
+        ].sort(() => Math.random() - 0.5)
+        );
+        console.log(Questions[index].correctAnswer)
+    }
+  };
 
   const next = () => {
-    if (index < (Questions.length - 1)) {
+    if (index < Questions.length - 1) {
       setindex(index + 1);
-      const allElements = document.querySelectorAll('li');
+      const allElements = document.querySelectorAll("li");
       allElements.forEach((element) => {
-        element.classList.remove('correct');
-        element.classList.remove('galat');
+        element.classList.remove("correct");
+        element.classList.remove("galat");
       });
-      setclicked(false)
-
-
+      setclicked(false);
     } else {
-      setfinished(true)
+      setfinished(true);
     }
-    setProgress(progress + 10)
-  }
+    setProgress(progress + 10);
+  };
 
-
-
-const restart= ()=>{
-  setindex(0)
-  setscore(0)
-  setfinished(false)
-  setclicked(false)
-
-}
-
-
-
-
-
-
+  const restart = () => {
+    setindex(0);
+    setscore(0);
+    setfinished(false);
+    setclicked(false);
+  };
 
   return (
     <>
-     <LoadingBar
-        color='#f11946'
+      <LoadingBar
+        color="#f11946"
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
-      {isLoading ?
-        <div class="spinner-border text-danger" id='spinner' role="status">
+      {isLoading ? (
+        <div class="spinner-border text-danger" id="spinner" role="status">
           <span class="sr-only"></span>
         </div>
-        :
+      ) : (
         <div className="container">
           <span>
-          <h1>Quiz App</h1>
-          <h2> {!isFinished ? `Category   :   ${Questions[index].category}`:""}</h2>
+            <h1>Quiz App</h1>
+            <h2>
+              {" "}
+              {!isFinished ? `Category   :   ${Questions[index].category}` : ""}
+            </h2>
           </span>
           <hr />
-         
 
-          {isFinished ?
-           <>
-            <h1>finished</h1>
-            <h2>you score {score} out of {Questions.length}</h2>
-            <button className='mt-3' onClick={restart}>Restart</button>
-           </>
-            :
+          {isFinished ? (
             <>
-              <h2>{index+1 + '. ' + Questions[index].question.text}</h2>
+              <h1>finished</h1>
+              <h2>
+                you score {score} out of {Questions.length}
+              </h2>
+              <button className="mt-3" onClick={restart}>
+                Restart
+              </button>
+            </>
+          ) : (
+            <>
+              <h2>{index + 1 + ". " + Questions[index].question.text}</h2>
               <ul>
-                <li onClick={checkAns}>{Questions[index].correctAnswer}</li>
-
-                {
-                  Questions[index].incorrectAnswers.map((e) => {
-                    return (<li onClick={checkAns}>{e}</li>)
-                  })
-                }
-
-
+                {options.map((option, idx) => (
+                  <li onClick={checkAns} key={idx}>
+                    {option}
+                  </li>
+                ))}
               </ul>
               <button onClick={next}>NEXT</button>
-              <div className="index">{index + 1} of {Questions.length} Question</div>
-
+              <div className="index">
+                {index + 1} of {Questions.length} Question
+              </div>
             </>
-
-          }
-
-
+          )}
         </div>
-      }
+      )}
     </>
   );
 }
